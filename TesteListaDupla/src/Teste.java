3ac6dfd5bf8d243fdfef7791f61dcab712d9bb02
NereuAugusto <nereubeckercampos711@gmail.com>
2022-03-11 12:20:32 -0300

    Minha primeira alteração

---
+++ TesteListaDupla/src/Teste.java
@@ -0,0 +1,12 @@
+
+public class Teste {
+	public static void main(String[] args) {
+		
+		Lista lista = new Lista();
+		lista.inserir("selmini", 123);
+		lista.inserir("Sandra", 456); //Ctrl + alt + seta pra baixo, duplica a linha
+		
+		lista.imprimir();
+	}
+
+}
